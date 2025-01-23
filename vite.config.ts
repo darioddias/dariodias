4447f6cb5323c497178378df2f74dba6cc601df7
Dario Dias <dario2dias@gmail.com>
2025-01-23 01:46:07 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/dariodias/',
+  base: '/', // Root path for custom domains
   plugins: [react()],
-
 });

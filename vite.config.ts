2dd9f95d551e507a2c9ff968f914008942bb4c10
Dario Dias <dario2dias@gmail.com>
2025-01-23 01:57:46 -0600

    fixed pathing

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/', // Root path for custom domains
+  base: '/', 
   plugins: [react()],
 });

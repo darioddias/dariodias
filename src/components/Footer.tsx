12f2d9913be89c29bb3bb8928b76a6bcadbe985f
Dario Dias <dario2dias@gmail.com>
2025-01-31 14:51:22 -0600

    footer changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,18 +1,19 @@
 const Footer = () => {
-    return (
-      <footer style={{
-        backgroundColor: 'black',
-        padding: '.5px 0',
-        textAlign: 'left',
-        color: '#1DB954',
-        fontSize: '12px',
-        borderTop: '1px solid #1DB954',
-        marginTop: '20px'
-      }}>
-        <p>thanks for checking this out :p - Dario</p>
-      </footer>
-    );
-  };
-  
-  export default Footer;
-  
+  return (
+    <footer style={{
+      backgroundColor: 'black',
+      padding: '5px 0', // Increased padding for visibility
+      textAlign: 'center', // Center aligns text
+      color: '#1DB954',
+      fontSize: '12px',
+      borderTop: '1px solid #1DB954',
+      width: '100%', // Ensures it spans full width
+      position: 'relative', // Keeps it within the document flow
+      bottom: 0 // Ensures it's at the bottom
+    }}>
+      <p>thanks for checking this out :p - Dario</p>
+    </footer>
+  );
+};
+
+export default Footer;
